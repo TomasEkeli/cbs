7606289d9a7aa9473b2f9b7e9c1ced6dee95c4ea
michael <michael@dolittle.com>
2018-02-10 09:53:15 +0100

    Add a Domain.Specs project and the first couple of Specs
    Setup MSpec and add first two specs as examples

--- Source/UserManagement/Domain/DataCollectors/AddDataCollector.cs
+++ Source/UserManagement/Domain/DataCollectors/AddDataCollector.cs
@@ -1,19 +1,20 @@
 using System;
 using Concepts;
+using doLittle.Commands;
 
 namespace Domain
 {
-    public class AddDataCollector
+    public class AddDataCollector : ICommand
     {
         //TODO: Update these properties to reflect what is needed for event. Remove PhoneNumber
         public Guid Id { get; set; }
-        public string FullName { get; set; }
-        public string DisplayName { get; set; }
-        public int YearOfBirth { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
         public Sex Sex { get; set; }
         public Guid NationalSociety { get; set; }
         public Language PreferredLanguage { get; set; }
-        public string GpsLocation { get; set; }
+        public Location GpsLocation { get; set; }
         public string MobilePhoneNumber { get; set; }
         public string Email { get; set; }
     }
